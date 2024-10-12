337f3534b4efc79a6c2031af82bf38b6d814a939
hvz1961 <vzkhametov@gmail.com>
2024-10-12 14:49:52 +0300

    add new const

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import pygame
-
+# Инициировали pygame
 pygame.init()
+# Создаем константы, неизменяемые величины, для экрана: ширина 800 пикселей, высота 600
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+# Создаем переменную для экрана, и далее функцию для экрана, чтобы экран появился
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+# Вводим название экрана
+pygame.display.set_caption('Игра Тир')
+# Загружаем изображение, картинку игры: создаем переменную, где будет храниться картинка
+icon = pygame.image.load()
 
 runnig = True
 while running:
